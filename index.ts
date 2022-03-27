c08aa1f0d12db93aecf0c0985cffaab7835c3ff7
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2022-03-27 10:54:30 -0300

    Using fetch to get API data

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import yargs from 'yargs';
-import axios from 'axios';
+import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 
 dotenv.config()
@@ -10,9 +10,15 @@ const argv = yargs(process.argv.slice(2)).options({
     city: { type: 'string' },
 }).argv;
 
-const location = `${argv['city']},${argv['state']}`
+const location = `${argv['city']},${argv['state']}`;
 
-axios.get(`${baseUrlWithKey}&city_name=${location}`).then(res => {
-    console.log(`
-    O clima em ${res.data.results.city} está ${res.data.results.description.toLowerCase()}. agora fazem ${res.data.results.temp}°C.`);
-});
+fetch(`${baseUrlWithKey}&city_name=${location}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(`O clima em ${data.results.city} está ${data.results.description.toLowerCase()}. agora fazem ${data.results.temp}°C.`);
+    });
+
+// (async () => {
+//     let res = await fetch(`${baseUrlWithKey}&city_name=${location}`);
+//     console.log(res.json());
+// })()
